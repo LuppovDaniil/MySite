85017f1cc495c552baeea7ae333af0b6f25fd96c
LuppovDaniil <luppov.dv@phystech.edu>
2023-04-27 02:40:43 +0300

    first commit

--- proj_mysite/urls.py
+++ proj_mysite/urls.py
@@ -1,4 +1,4 @@
-"""proj_maths URL Configuration
+"""proj_mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
